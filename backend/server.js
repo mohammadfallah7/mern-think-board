2d5ef30736df3abe64a79eb0eac67398cfc361f4
Mohammad Fallah <mohammadfallah.w@gmail.com>
2026-02-05 12:06:11 +0330

    feat: :sparkles: Create first REST API
    A get request

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+  res.send("You got 5 notes");
+});
+
 app.listen(3000, () => {
   console.log("Sever started on PORT:3000");
 });
