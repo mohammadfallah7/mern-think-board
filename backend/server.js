f9e5e5cf10c00fc2f4855c73d6c49f42e9cf9eb3
Mohammad Fallah <mohammadfallah.w@gmail.com>
2026-02-05 12:24:47 +0330

    feat: :sparkles: Create 4 endpoints successfully

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,19 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.send("You got 5 notes");
+  res.json({ message: "You get all notes successfully!" });
+});
+
+app.post("/api/notes", (req, res) => {
+  res.json({ message: "You create a note successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.json({ message: "You update a note successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.json({ message: "You delete a note successfully!" });
 });
 
 app.listen(3000, () => {
